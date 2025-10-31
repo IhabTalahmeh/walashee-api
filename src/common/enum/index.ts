2f02e5d35ce14de2d50cc560783272934e0d003a
Ihab Talahmeh <ihab.talahma@gmail.com>
2025-10-31 06:44:10 +0200

    Fixes

--- src/common/enum/index.ts
+++ src/common/enum/index.ts
@@ -3,3 +3,4 @@ export * from './role-type.enum';
 export * from './reviewable-type.enum';
 export * from './product-creator-type.enum';
 export * from './language-code.enum';
+export * from './verification-purpose.enum';
